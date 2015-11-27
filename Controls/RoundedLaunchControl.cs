f9f8ac45be23faa19ed310e6e0c528945a0ff331
jasper22 <jjasper22@gmail.com>
2015-11-27 23:46:17 +0200

    Finished the 'EtchedWindow' Background

---
+++ Controls/RoundedLaunchControl.cs
@@ -0,0 +1,23 @@
+﻿
+namespace RandomUI.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class RoundedLaunchControl : Control
+    {
+        static RoundedLaunchControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedLaunchControl), new FrameworkPropertyMetadata(typeof(RoundedLaunchControl)));
+        }
+
+        public RoundedLaunchControl()
+        {
+        }
+    }
+}
