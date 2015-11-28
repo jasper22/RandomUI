a75c86088fe72805d0c0e9b334be1cfa806e0d52
jasper22 <jjasper22@gmail.com>
2015-11-28 19:00:03 +0200

    Added GreyHalfTransperant VisualBrush

---
+++ Controls/ValueConverters/TranslateTransformToRectViewboxVisualBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿
+namespace RandomUI.Controls.ValueConverters
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    ///     http://charly-studio.com/blog/aero-effect-blurry-transparency-in-wpf/
+    /// </remarks>
+    [ValueConversion(typeof(TranslateTransform), typeof(Rect))]
+    public class TranslateTransformToRectViewboxVisualBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var translate = value as TranslateTransform;
+            if (translate != null)
+            {
+                return new Rect(translate.X, translate.Y, 0d, 0d);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
