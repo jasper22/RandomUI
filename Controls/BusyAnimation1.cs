f1339246ea8e3d5fb29bfece7290c75bd14af04d
jasper22 <jjasper22@gmail.com>
2015-11-29 11:11:24 +0200

    Added BusyAnimation1 control

---
+++ Controls/BusyAnimation1.cs
@@ -0,0 +1,104 @@
+﻿
+namespace RandomUI.Controls
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media.Animation;
+    using Helpers;
+
+    /// <summary>
+    /// The <c>BusyAnimation1</c>
+    /// </summary>
+    public class BusyAnimation1 : Control
+    {
+        private const double STORYBOARD_DEFAULT_MAX_VALUE = 200;
+
+        private const string TemplateRoot = "templateRoot";
+
+        private const string AnimatedRoot = "animatedRoot";
+
+        public static readonly DependencyProperty AnimationEasingFunctionProperty = DependencyProperty.Register("AnimationEasingFunction", typeof(IEasingFunction), typeof(BusyAnimation1), new PropertyMetadata(null));
+
+
+        private Grid animatedRoot, templateRoot;
+
+        /// <summary>
+        /// Initializes the <see cref="BusyAnimation1"/> class.
+        /// </summary>
+        static BusyAnimation1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyAnimation1), new FrameworkPropertyMetadata(typeof(BusyAnimation1)));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BusyAnimation1"/> class.
+        /// </summary>
+        public BusyAnimation1()
+        {
+            this.Loaded += BusyAnimation1_Loaded;
+        }
+
+        /// <summary>
+        /// Called when [apply template].
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.templateRoot = GetTemplateChild(TemplateRoot) as Grid;
+            this.animatedRoot = GetTemplateChild(AnimatedRoot) as Grid;
+        }
+
+        /// <summary>
+        /// Handles the Loaded event of the BusyAnimation1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void BusyAnimation1_Loaded(object sender, RoutedEventArgs e)
+        {
+            var stateGroup = VisualStatesManagerHelper.TryGetVisualStateGroup(this.animatedRoot, "DefaultVisualStates");
+
+            if (stateGroup != null)
+            {
+                var vState1 = stateGroup.States.OfType<VisualState>().SingleOrDefault(state => state.Name.Equals("Working", StringComparison.OrdinalIgnoreCase));
+                if (vState1 != null)
+                {
+                    var tmpMoveEleipsesAnimation = vState1.Storyboard;
+                    if (tmpMoveEleipsesAnimation != null)
+                    {
+                        var moveEleipsesAnimation = vState1.Storyboard.Clone();
+
+                        foreach (DoubleAnimation child in moveEleipsesAnimation.Children)
+                        {
+                            if ((child.To.HasValue == true) && (child.To.Value == STORYBOARD_DEFAULT_MAX_VALUE))
+                            {
+                                child.To = this.templateRoot.ActualWidth - 70;
+                            }
+
+                            if ((child.From.HasValue == true) && (child.From.Value == STORYBOARD_DEFAULT_MAX_VALUE))
+                            {
+                                child.From = this.templateRoot.ActualWidth - 70;
+                            }
+                        }
+
+                        moveEleipsesAnimation.Begin(this.templateRoot);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the animation easing function.
+        /// </summary>
+        /// <value>
+        /// The animation easing function.
+        /// </value>
+        public IEasingFunction AnimationEasingFunction
+        {
+            get { return (IEasingFunction)GetValue(AnimationEasingFunctionProperty); }
+            set { SetValue(AnimationEasingFunctionProperty, value); }
+        }
+    }
+}
---
+++ Controls/Helpers/VisualStatesManagerHelper.cs
@@ -0,0 +1,98 @@
+﻿
+namespace RandomUI.Controls.Helpers
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Helper object for Window VisualState's
+    /// </summary>
+    public class VisualStatesManagerHelper
+    {
+        /// <summary>
+        /// Function will return the very 'base' <see cref="FrameworkElement"/> for provided <paramref name="dependencyObject"/>
+        /// </summary>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <returns><see cref="FrameworkElement"/> if possible; otherwise <c>null</c></returns>
+        internal static FrameworkElement GetImplementationRoot(DependencyObject dependencyObject)
+        {
+            Debug.Assert(dependencyObject != null, "DependencyObject should not be null.");
+
+            return (VisualTreeHelper.GetChildrenCount(dependencyObject) == 1) ?
+                VisualTreeHelper.GetChild(dependencyObject, 0) as FrameworkElement :
+                null;
+        }
+
+        /// <summary>
+        /// Tries the get <see cref="VisualStateGroup"/> from provided <paramref name="dependencyObject"/>
+        /// </summary>
+        /// <param name="dependencyObject">The dependency object.</param>
+        /// <param name="groupName">Name of the group.</param>
+        /// <returns><see cref="VisualStateGroup"/> object if possible; otherwise <c>null</c></returns>
+        internal static VisualStateGroup TryGetVisualStateGroup(DependencyObject dependencyObject, string groupName)
+        {
+            FrameworkElement root = GetImplementationRoot(dependencyObject);
+            if (root == null)
+            {
+                return null;
+            }
+
+            return TryGetVisualStateGroup(root, groupName);
+        }
+
+        /// <summary>
+        /// Tries the get <see cref="VisualStateGroup"/> from provided <paramref name="root"/>
+        /// </summary>
+        /// <param name="root">The root element</param>
+        /// <param name="groupName">Name of the group.</param>
+        /// <returns><see cref="VisualStateGroup"/> object if possible; otherwise <c>null</c></returns>
+        internal static VisualStateGroup TryGetVisualStateGroup(FrameworkElement root, string groupName)
+        {
+            return VisualStateManager.GetVisualStateGroups(root)
+                                     .OfType<VisualStateGroup>()
+                                     .FirstOrDefault(group => string.CompareOrdinal(groupName, @group.Name) == 0);
+        }
+
+        /// <summary>
+        /// Goes to state.
+        /// </summary>
+        /// <param name="targetControl">The target control.</param>
+        /// <param name="visualStatesGroupName">Name of the visual states group.</param>
+        /// <param name="targetState">State of the target.</param>
+        /// <param name="useTransition">if set to <c>true</c> [use transition].</param>
+        internal static void GoToState(FrameworkElement targetControl, string visualStatesGroupName, string targetState, bool useTransition = true)
+        {
+            if (StateExist(targetControl, visualStatesGroupName, targetState) == true)
+            {
+                VisualStateManager.GoToState(targetControl, targetState, useTransition);
+            }
+            else
+            {
+                // VisualState for this 'target state' is not defined in XAML
+            }
+
+        }
+
+        /// <summary>
+        /// Function will check if any of <see cref="VisualState"/> that exist in <paramref name="targetControl"/> does contain provided <paramref name="stateName"/>
+        /// </summary>
+        /// <param name="targetControl">The target control.</param>
+        /// <param name="visualStatesGroupName">Name of the visual states group.</param>
+        /// <param name="stateName">Name of the state.</param>
+        /// <returns><c>True</c> if exist; otherwise <c>false</c></returns>
+        internal static bool StateExist(FrameworkElement targetControl, string visualStatesGroupName, string stateName)
+        {
+            var statesGroup = TryGetVisualStateGroup(targetControl, visualStatesGroupName);
+            if (statesGroup == null)
+            {
+                // This VisualStateGroup is not defined in XAML !
+                return false;
+            }
+
+            return statesGroup.States.OfType<VisualState>().Any(state => state.Name.StartsWith(stateName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
